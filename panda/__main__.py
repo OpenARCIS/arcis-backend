abdbc817d94743e4b0e8da785be59c77d4a5ae35
Vinayak Mundakkal <76476634+vinayak-7-0-3@users.noreply.github.com>
2026-02-15 16:34:36 +0530

    Interrupt handling doe auto workflow

--- panda/__main__.py
+++ panda/__main__.py
@@ -10,6 +10,7 @@ from panda.router.gmail import gmail_router
 from panda.router.settings import settings_router
 from panda.router.calendar import calendar_router
 from panda.router.chat import chat_router
+from panda.router.auto_flow import auto_flow_router
 from panda.router.user_status import user_status_router
 from panda.router.token_tracker import token_tracker_router
 from .database.mongo.connection import mongo
@@ -59,6 +60,7 @@ api_server.include_router(gmail_router)
 api_server.include_router(settings_router)
 api_server.include_router(calendar_router)
 api_server.include_router(chat_router)
+api_server.include_router(auto_flow_router)
 api_server.include_router(user_status_router)
 api_server.include_router(token_tracker_router)
 
